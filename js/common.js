9c3f40e132777c11cc91767344ea326172f40866
Orxan <baku_orxan17@mail.ru>
2018-05-30 22:18:54 +0400

    widget done

--- js/common.js
+++ js/common.js
@@ -1,3 +1,11 @@
-$('.drop').on('click', function(){
-    $('.dropdown-content').toggleClass('transform-scale');
+$( document ).ready(function() {
+    $('.drop').on('click', function () {
+        $('.dropdown-content').toggleClass('transform-scale');
+    });
+
+   // var height = $('.single').outerHeight();
+   //
+   //  $('#main .col-md-6 .item').height(height / 2 - 5);
+
 });
+
