221a87b3374fb7f979d2f2330cdc4c28d3b12476
Orxan <baku_orxan17@mail.ru>
2018-06-02 09:49:50 +0400

    slick and respons

--- js/common.js
+++ js/common.js
@@ -3,43 +3,7 @@ $( document ).ready(function() {
         $('.dropdown-content').toggleClass('transform-scale');
     });
 
-   // var height = $('.single').outerHeight();
-   //
-   //  $('#main .col-md-6 .item').height(height / 2 - 5);
 
-    $('.responsive').slick({
-        dots: false,
-        infinite: true,
-        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    infinite: true,
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
 
 
 });
